c63fd8f6a08a618f51ebd1534d09e69629636203
森重ゆう <morishige_yu@morishigeyuu-no-MacBook-Air.local>
2017-10-31 19:04:38 +0900

    修正5

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ end
   content = Faker::Beer.name
   Topic.create!(title: title,
                content: content,
-               user_id: "#{t}",
+               user_id: "#{t+1}",
               )
 
 end
